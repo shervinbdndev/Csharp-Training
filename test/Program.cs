bc5c1802a25d0729cf98e4588118941583e5c77b
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2024-10-15 17:13:34 +0330

    Commited

---
+++ test/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Test
+{
+    class Person
+    {
+        public string? first_name
+        {
+            get; set;
+        }
+        public string? last_name
+        {
+            get; set;
+        }
+        public int? age
+        {
+            get; set;
+        }
+
+        public Person(string? first_name, string? last_name, int? age)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.age = age;
+        }
+
+        public string fullname()
+        {
+            return $"{this.first_name} {this.last_name}";
+        }
+    }
+
+    class User : Person
+    {
+        public string? email
+        {
+            get; set;
+        }
+
+        public string? password
+        {
+            get; set;
+        }
+        public User(string? first_name, string? last_name, int? age, string? email, string? password) : base(first_name, last_name, age)
+        {
+            this.email = email;
+            this.password = password;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            User user = new User("shervin", "badanara", 21, "shervin2234@gmail.com", "shb12345");
+            System.Console.WriteLine(user.fullname());
+            Console.ReadKey();
+        }
+    }
+}
+
